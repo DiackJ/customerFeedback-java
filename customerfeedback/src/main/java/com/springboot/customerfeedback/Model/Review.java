9c798464ef0c0803d789a744961ef9c9d60ecb35
Jessica <diackjessica5@gmail.com>
2025-06-16 13:25:08 -0400

    feat: implement customer review entity

--- customerfeedback/src/main/java/com/springboot/customerfeedback/Model/Review.java
+++ customerfeedback/src/main/java/com/springboot/customerfeedback/Model/Review.java
@@ -1,4 +1,24 @@
 package com.springboot.customerfeedback.Model;
 
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "reviews")
 public class Review {
+    @Id
+    private ObjectId id;
+    private String customer;
+    private String vendor;
+    private String product;
+    private double rating;
+    private String feedback;
+
+    public Review(){}
+
+    public void setCustomer(String customer){this.customer = customer;}
+    public void setVendor(String vendor){this.vendor = vendor;}
+    public void setProduct(String product){this.product = product;}
+    public void setRating(double rating){this.rating = rating;}
+    public void setFeedback(String feedback){this.feedback = feedback;}
 }
