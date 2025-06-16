af4088607004a82e7968e223c3210057c20e1df0
Jessica <diackjessica5@gmail.com>
2025-06-16 17:59:35 -0400

    feat: implement fallback EnvLoader class to ensure env values configure at startup

--- customerfeedback/src/main/java/com/springboot/customerfeedback/Config/EnvLoader.java
+++ customerfeedback/src/main/java/com/springboot/customerfeedback/Config/EnvLoader.java
@@ -1,4 +1,19 @@
 package com.springboot.customerfeedback.Config;
 
-public class EnvLoader {
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+
+//fallback class to ensure env values are loaded upon program initialization
+public class EnvLoader implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+    @Override
+    public void initialize(ConfigurableApplicationContext appContext){
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+        dotenv.entries().forEach(e -> {
+            if(System.getProperty(e.getKey()) == null && System.getenv(e.getKey()) == null){
+                System.setProperty(e.getKey(), e.getValue());
+            }
+        });
+    }
 }
