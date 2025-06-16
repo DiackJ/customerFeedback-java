c149ac0dccc53ec0831641b9a10f916290229ae0
Jessica <diackjessica5@gmail.com>
2025-06-16 13:41:37 -0400

    initiate review entity repository

--- customerfeedback/src/main/java/com/springboot/customerfeedback/DTO/PageDTO.java
+++ customerfeedback/src/main/java/com/springboot/customerfeedback/DTO/PageDTO.java
@@ -1,4 +1,35 @@
 package com.springboot.customerfeedback.DTO;
 
-public class PagetDTO {
+import com.springboot.customerfeedback.Model.Review;
+import org.springframework.data.domain.Page;
+
+import java.util.*;
+
+public class PageDTO {
+    private int totalDocs;
+    private boolean isFirstPage;
+    private boolean isLastPage;
+    private int currentPage;
+    private int totalPages;
+    private List<Review> reviews;
+    private Page<Review> pages;
+
+    public PageDTO(){
+        this.reviews = new ArrayList<>();
+    }
+
+    public int getTotalDocs(){return this.totalDocs;}
+    public void setTotalDocs(int totalDocs){this.totalDocs = totalDocs;}
+
+    public void setIsFirstPage(boolean isFirstPage){this.isFirstPage = isFirstPage;}
+
+    public void setIsLastPage(boolean isLastPage){this.isLastPage = isLastPage;}
+
+    public int getCurrentPage(){return this.currentPage;}
+    public void setCurrentPage(int currentPage){this.currentPage = currentPage;}
+
+    public int getTotalPages(){return this.totalPages;}
+    public void setTotalPages(int totalPages){this.totalPages = totalPages;}
+
+    public void setPages(Page<Review> pages){this.pages = pages;}
 }
--- customerfeedback/src/main/java/com/springboot/customerfeedback/Repository/ReviewRepository.java
+++ customerfeedback/src/main/java/com/springboot/customerfeedback/Repository/ReviewRepository.java
@@ -1,4 +1,10 @@
 package com.springboot.customerfeedback.Repository;
 
-public interface ReviewRepository {
+import com.springboot.customerfeedback.Model.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
 }
